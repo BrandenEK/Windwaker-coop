e728dfc5c96f86950dd04c579a0f742ff51272fb
Branden <44546325+BrandenEK@users.noreply.github.com>
2021-08-08 12:20:37 -0400

    done

--- CustomStructs.cs
+++ CustomStructs.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using Newtonsoft.Json;
 
-namespace Windwaker_Rammer
+namespace Windwaker_coop
 {
     struct MemoryLocation
     {
