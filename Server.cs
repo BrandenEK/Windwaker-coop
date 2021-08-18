9776ce0318cd9713917ebcb158edd7e28f28b2a9
Branden <44546325+BrandenEK@users.noreply.github.com>
2021-08-18 16:04:27 -0400

    base compare function

--- Server.cs
+++ Server.cs
@@ -29,8 +29,44 @@ namespace Windwaker_coop
             //reverse byte endian first!!
             //if new data add it to host list
             //send new memoryLocation to everyone & notification
-            sendNotification("Server is giving out 10 max hearts");
-            sendNewMemoryLocation(2, 40);
+            //now has to check if player has new number and if host has new number
+            //maybe between low/high values
+
+            int byteListIndex = 0;
+            uint hostNumber = 0;
+            uint playerNumber = 0;
+
+            for (int locationListIndex = 0; locationListIndex < mr.memoryLocations.Count; locationListIndex++)
+            {
+                MemoryLocation memLoc = mr.memoryLocations[locationListIndex];
+                hostNumber = getNumberFromByteList(hostdata, byteListIndex, memLoc.size);
+                playerNumber = getNumberFromByteList(playerData, byteListIndex, memLoc.size);
+
+                if (playerNumber != hostNumber)
+                {
+                    switch (memLoc.compareId)
+                    {
+                        case 0:
+                            if (playerNumber > hostNumber)
+                            {
+                                //player --> host
+                            }
+                            else
+                            {
+                                //host --> player
+                            }
+                            break;
+                        default:
+                            Program.displayError("Invalid compareId");
+                            break;
+                    }
+                }
+
+                byteListIndex += memLoc.size;
+            }
+
+            sendNotification("Server is giving out more max hearts");
+            sendNewMemoryLocation(2, 5);
         }
 
         public void Start()
@@ -50,26 +86,25 @@ namespace Windwaker_coop
             }
         }
 
-        protected override void sendNewMemoryLocation(uint memLocIndex, uint newValue)
+        protected override void sendNewMemoryLocation(short memLocIndex, uint newValue)
         {
             List<byte> toSend = new List<byte>();
             toSend.AddRange(BitConverter.GetBytes(memLocIndex));
-            toSend.AddRange(BitConverter.GetBytes(newValue));
+            toSend.AddRange(getByteArrayFromNumber(newValue, mr.memoryLocations[memLocIndex].size));
             toSend.Add(118);
             byte[] toSendArray = toSend.ToArray();
 
-            foreach (byte b in toSend)
-                Console.Write(b + " ");
-
             foreach (string ip in clientIps)
             {
-                Send(ip, toSendArray);
+                //if (ip != currIp) //some data will go to curr ip, some will go to only others
+                    Send(ip, toSendArray);
             }
         }
         protected override void sendNotification(string notification)
         {
             foreach (string ip in clientIps)
             {
+                //send name notification to else, send you to this player
                 Send(ip, Encoding.UTF8.GetBytes(notification + 'n'));
             }
         }
