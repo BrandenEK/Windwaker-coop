c81a550d405c1f745df45f786293810f936fb0c8
Branden <44546325+BrandenEK@users.noreply.github.com>
2022-03-19 10:02:01 -0400

    Slight improvement to command loop

--- Windwaker-coop/Program.cs
+++ Windwaker-coop/Program.cs
@@ -118,7 +118,7 @@ namespace Windwaker_coop
                     debugOuput += $" '{word}'";
                 Output.debug(debugOuput, 2);
 
-                //Processes multiword command
+                //Processes multiword command - not sure if this is needed
                 if (words.Length > 1)
                 {
                     lastCommand = words[0];
@@ -132,157 +132,158 @@ namespace Windwaker_coop
                     }
                 }
 
-                //If the command is valid, check which one and use it
-                if (validCommand)
+                if (!validCommand)
                 {
-                    string response = "";
+                    Output.text("Syntax error with inputted command.", ConsoleColor.Yellow);
+                    continue;
+                }
 
-                    if (currUser.GetType() == typeof(Client))
+                //If the command is valid, check which one and use it
+                string response = "";
+
+                if (currUser.GetType() == typeof(Client))
+                {
+                    //Client commands
+                    Client client = (Client)currUser;
+
+                    switch (lastCommand)
                     {
-                        //Client commands
-                        Client client = (Client)currUser;
+                        case "help":
+                            //Displays the available client commands
+                            response = "Available client commands:\npause - temporarily disables syncing to and from the host\nunpause - resumes syncing to and from the host\n" +
+                                "stop - ends syncing and closes the application\nsay [message] - sends a message to everyone in the server\n" +
+                                "give [item] [number] - gives player the specified item (If cheats are enabled)\nping - tests the delay between client and server\n" +
+                                "help - lists available commands";
+                            break;
+                        case "pause":
+                            //command not implemented yet
+                            response = "command not implemented yet";
+                            break;
+                        case "unpause":
+                            //command not implemented yet
+                            response = "command not implemented yet";
+                            break;
 
-                        switch (lastCommand)
-                        {
-                            case "help":
-                                //Displays the available client commands
-                                response = "Available client commands:\npause - temporarily disables syncing to and from the host\nunpause - resumes syncing to and from the host\n" +
-                                    "stop - ends syncing and closes the application\nsay [message] - sends a message to everyone in the server\n" +
-                                    "give [item] [number] - gives player the specified item (If cheats are enabled)\nping - tests the delay between client and server\n" +
-                                    "help - lists available commands";
-                                break;
-                            case "pause":
-                                //command not implemented yet
-                                response = "command not implemented yet";
-                                break;
-                            case "unpause":
-                                //command not implemented yet
-                                response = "command not implemented yet";
-                                break;
-
-                            case "say":
-                                //Takes in a message and sends it to everyone else in the game
-                                if (words.Length > 1)
-                                {
-                                    string text = "";
-                                    for (int i = 1; i < words.Length; i++)
-                                        text += words[i] + " ";
-                                    client.sendTextMessage(text);
-                                    response = "Message sent";
-                                }
-                                else
-                                    response = "Command 'say' takes at least 1 argument!";
-                                break;
-                            case "ping":
-                                //Sends a test to the server to determine the delay
-                                response = "Sending delay test!";
-                                client.sendDelayTest();
-                                break;
-                            case "give":
-                                //Gives the player a specified item
-                                if (currCheater != null)
-                                {
-                                    response = currCheater.processCommand(words);
-                                }
-                                else
-                                    Output.error("Cheater object has not been initialized");
-                                break;
-                            case "stop": break;
-                            default:
-                                response = "Command '" + lastCommand + "' not valid.";
-                                break;
-                        }
+                        case "say":
+                            //Takes in a message and sends it to everyone else in the game
+                            if (words.Length > 1)
+                            {
+                                string text = "";
+                                for (int i = 1; i < words.Length; i++)
+                                    text += words[i] + " ";
+                                client.sendTextMessage(text);
+                                response = "Message sent";
+                            }
+                            else
+                                response = "Command 'say' takes at least 1 argument!";
+                            break;
+                        case "ping":
+                            //Sends a test to the server to determine the delay
+                            response = "Sending delay test!";
+                            client.sendDelayTest();
+                            break;
+                        case "give":
+                            //Gives the player a specified item
+                            if (currCheater != null)
+                            {
+                                response = currCheater.processCommand(words);
+                            }
+                            else
+                                Output.error("Cheater object has not been initialized");
+                            break;
+                        case "stop": break;
+                        default:
+                            response = "Command '" + lastCommand + "' not valid.";
+                            break;
                     }
-                    else if (currUser.GetType() == typeof(Server))
-                    {
-                        //server commands
-                        Server server = (Server)currUser;
+                }
+                else if (currUser.GetType() == typeof(Server))
+                {
+                    //server commands
+                    Server server = (Server)currUser;
 
-                        switch (lastCommand)
-                        {
-                            case "help":
-                                //Displays the available server commands
-                                response = "Available server commands:\nlist - lists all of the currently connected players\n" +
-                                    "reset - resets the host to default values\n" +
-                                    "kick [type] [Name or IpPort] - kicks the speciifed Name or IpPort from the game\nstop - ends syncing and closes the application\n" +
-                                    "help - lists available commands";
-                                break;
-                            case "list":
-                                //List the ip addresses in the server
-                                response = "Connected players:\n";
-                                foreach (string ip in server.clientIps.Keys)
+                    switch (lastCommand)
+                    {
+                        case "help":
+                            //Displays the available server commands
+                            response = "Available server commands:\nlist - lists all of the currently connected players\n" +
+                                "reset - resets the host to default values\n" +
+                                "kick [type] [Name or IpPort] - kicks the speciifed Name or IpPort from the game\nstop - ends syncing and closes the application\n" +
+                                "help - lists available commands";
+                            break;
+                        case "list":
+                            //List the ip addresses in the server
+                            response = "Connected players:\n";
+                            foreach (string ip in server.clientIps.Keys)
+                            {
+                                response += $"{server.clientIps[ip].name} ({ip})\n";
+                            }
+                            if (server.clientIps.Count < 1)
+                                response += "none\n";
+                            response = response.Substring(0, response.Length - 1);
+                            break;
+                        case "reset":
+                            //resets the server to default values
+                            server.setServerToDefault();
+                            response = "Server data has been reset to default!";
+                            server.sendNotification("Server data has been reset to default!", true);
+                            break;
+                        case "kick":
+                            //kicks the inputted player's ipPort or name from the game
+                            if (words.Length == 3)
+                            {
+                                if (words[1] == "name" || words[1] == "n")
                                 {
-                                    response += $"{server.clientIps[ip].name} ({ip})\n";
-                                }
-                                if (server.clientIps.Count < 1)
-                                    response += "none\n";
-                                response = response.Substring(0, response.Length - 1);
-                                break;
-                            case "reset":
-                                //resets the server to default values
-                                server.setServerToDefault();
-                                response = "Server data has been reset to default!";
-                                server.sendNotification("Server data has been reset to default!", true);
-                                break;
-                            case "kick":
-                                //kicks the inputted player's ipPort or name from the game
-                                if (words.Length == 3)
-                                {
-                                    if (words[1] == "name" || words[1] == "n")
+                                    //Change to simple lookup once names are individualized
+                                    int numFound = 0;
+                                    foreach (string ip in server.clientIps.Keys)
                                     {
-                                        //Change to simple lookup once names are individualized
-                                        int numFound = 0;
-                                        foreach (string ip in server.clientIps.Keys)
+                                        if (server.clientIps[ip].name == words[2])
                                         {
-                                            if (server.clientIps[ip].name == words[2])
-                                            {
-                                                server.kickPlayer(ip);
-                                                response += "Player '" + words[2] + "' has been kicked from the game!";
-                                                numFound++;
-                                            }
+                                            server.kickPlayer(ip);
+                                            response += "Player '" + words[2] + "' has been kicked from the game!";
+                                            numFound++;
                                         }
-                                        if (numFound == 0)
-                                            response = "Player '" + words[2] + "' does not exist in the game!";
                                     }
-                                    else if (words[1] == "ip" || words[1] == "i")
+                                    if (numFound == 0)
+                                        response = "Player '" + words[2] + "' does not exist in the game!";
+                                }
+                                else if (words[1] == "ip" || words[1] == "i")
+                                {
+                                    if (server.clientIps.ContainsKey(words[2]))
                                     {
-                                        if (server.clientIps.ContainsKey(words[2]))
-                                        {
-                                            server.kickPlayer(words[2]);
-                                            response = "IpPort '" + words[2] + "' has been kicked from the game!";
-                                        }
-                                        else
-                                            response = "IpPort '" + words[2] + "' does not exist in the game!";
+                                        server.kickPlayer(words[2]);
+                                        response = "IpPort '" + words[2] + "' has been kicked from the game!";
                                     }
                                     else
-                                    {
-                                        response =  "Invalid type.  Must be either 'name' or 'ip'";
-                                    }
+                                        response = "IpPort '" + words[2] + "' does not exist in the game!";
                                 }
                                 else
-                                    response =  "Command 'kick' takes 2 arguments!";
-                                break;
-                            case "ban":
-                                //command not implemented yet
-                                response =  "command not implemented yet";
-                                break;
-                            case "stop": break;
-                            default:
-                                response =  "Command '" + lastCommand + "' not valid.";
-                                break;
-                        }
+                                {
+                                    response = "Invalid type.  Must be either 'name' or 'ip'";
+                                }
+                            }
+                            else
+                                response = "Command 'kick' takes 2 arguments!";
+                            break;
+                        case "ban":
+                            //command not implemented yet
+                            response = "command not implemented yet";
+                            break;
+                        case "stop": break;
+                        default:
+                            response = "Command '" + lastCommand + "' not valid.";
+                            break;
                     }
-                    else
-                    {
-                        Output.error("User is neither a server nor a client.  WTH");
-                        EndProgram();
-                    }
-
-                    if (response != "")
-                        Output.text(response, ConsoleColor.Yellow);
                 }
                 else
-                    Output.text("Syntax error with inputted command.", ConsoleColor.Yellow);
+                {
+                    Output.error("User is neither a server nor a client.  WTH");
+                    EndProgram();
+                }
+
+                if (response != "")
+                    Output.text(response, ConsoleColor.Yellow);
             }
         }
 
