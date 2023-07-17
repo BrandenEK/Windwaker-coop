09feeb688b6ef9d97e4ce713be6773dcce6324d6
Branden Wood <brandenek03912@hotmail.com>
2023-07-17 17:40:40 -0400

    Client: Validate and save all 5 input fields

--- Windwaker.Multiplayer.Client/ClientSettings.cs
+++ Windwaker.Multiplayer.Client/ClientSettings.cs
@@ -3,23 +3,38 @@ namespace Windwaker.Multiplayer.Client
 {
     internal class ClientSettings
     {
-        public readonly string playerName;
+        public const string DEFAULT_PLAYER_NAME = "New player";
+        public const string DEFAULT_GAME_NAME = "Windwaker";
+        public const string DEFAULT_SERVER_IP = "127.0.0.1";
+        public const int DEFAULT_SERVER_PORT = 8989;
 
-        public readonly string gameName;
+        private readonly string _playerName;
+        public string PlayerName => _playerName ?? DEFAULT_PLAYER_NAME;
+        public string TruePlayerName => _playerName;
 
-        public readonly string serverIp;
+        private readonly string _gameName;
+        public string GameName => _gameName ?? DEFAULT_GAME_NAME;
+        public string TrueGameName => _gameName;
 
-        public readonly int serverPort;
+        private readonly string _serverIp;
+        public string ServerIp => _serverIp ?? DEFAULT_SERVER_IP;
+        public string TrueServerIp => _serverIp;
 
-        public readonly string password;
+        private readonly int _serverPort;
+        public int ServerPort => _serverPort > 0 ? _serverPort : DEFAULT_SERVER_PORT;
+        public int TrueServerPort => _serverPort;
+
+        private readonly string _password;
+        public string Password => _password;
+
 
         public ClientSettings(string playerName, string gameName, string serverIp, int serverPort, string password)
         {
-            this.playerName = playerName;
-            this.gameName = gameName;
-            this.serverIp = serverIp;
-            this.serverPort = serverPort;
-            this.password = password;
+            _playerName = playerName;
+            _gameName = gameName;
+            _serverIp = serverIp;
+            _serverPort = serverPort;
+            _password = password;
         }
     }
 }
--- Windwaker.Multiplayer.Client/MainForm.cs
+++ Windwaker.Multiplayer.Client/MainForm.cs
@@ -30,51 +30,9 @@ namespace Windwaker.Multiplayer.Client
             }
             else
             {
-                if (ValidateAndUpdateSettings())
-                {
-                    _client.Connect(_settings.serverIp, _settings.serverPort);
-                }
-                else
-                {
-                    Log("Enter valid data for the input fields!");
-                }
+                _settings = ValidateInputFields();
+                _client.Connect(_settings.ServerIp, _settings.ServerPort);
             }
-        }
-
-        /// <summary>
-        /// Ensures that the input fields contain valid info, and if so, update the client settings
-        /// </summary>
-        /// <returns></returns>
-        private bool ValidateAndUpdateSettings()
-        {
-            // Ensure that a name is present
-            string playerName = playerNameField.Text.Trim();
-            if (playerName.Length == 0)
-            {
-                return false;
-            }
-
-            // The game will always be ww for now
-            string gameName = "Windwaker";
-
-            // Ensure that an ip is present and doesn't contain a colon
-            string serverIp = serverIpField.Text.Trim();
-            if (serverIp.Length == 0 || serverIp.Contains(':'))
-            {
-                return false;
-            }
-
-            // Ensure that the port is a valid number
-            if (!int.TryParse(serverPortField.Text.Trim(), out int serverPort))
-            {
-                return false;
-            }
-
-            // No validation for password, it doesn't need to be present
-            string password = passwordField.Text.Trim();
-
-            _settings = new ClientSettings(playerName, gameName, serverIp, serverPort, password);
-            return true;
         }
 
         /// <summary>
@@ -86,22 +44,61 @@ namespace Windwaker.Multiplayer.Client
         }
 
         /// <summary>
+        /// Ensures that the input fields contain valid info, and if so, update the client settings
+        /// </summary>
+        private ClientSettings ValidateInputFields()
+        {
+            // Ensure that player name has a valid length
+            string playerName = playerNameField.Text.Trim();
+            if (playerName.Length < 1 || playerName.Length > 16)
+            {
+                playerName = null;
+            }
+
+            // The game will always be ww for now
+            string gameName = null;
+
+            // Ensure that server ip has a valid length and doesn't contain a colon
+            string serverIp = serverIpField.Text.Trim();
+            if (serverIp.Length < 1 || serverIp.Length > 16 || serverIp.Contains(':'))
+            {
+                serverIp = null;
+            }
+
+            // Ensure that the port is a valid number
+            if (!int.TryParse(serverPortField.Text.Trim(), out int serverPort) || serverPort < 0)
+            {
+                serverPort = 0;
+            }
+
+            // Ensure that password is empty or has a valid length
+            string password = passwordField.Text.Trim();
+            if (password.Length > 16)
+            {
+                password = null;
+            }
+
+            playerNameField.Text = playerName;
+            gameNameField.Text = gameName;
+            serverIpField.Text = serverIp;
+            serverPortField.Text = serverPort > 0 ? serverPort.ToString() : null;
+            passwordField.Text = password;
+
+            return new ClientSettings(playerName, gameName, serverIp, serverPort, password);
+        }
+
+        /// <summary>
         /// When the form is opened, load the last used ip from settings
         /// </summary>
         private void OnFormOpen(object sender, EventArgs e)
         {
-            string playerName = Properties.Settings.Default.playerName;
-            string gameName = Properties.Settings.Default.gameName;
-            string serverIp = Properties.Settings.Default.serverIp;
-            int serverPort = Properties.Settings.Default.serverPort;
-            string password = Properties.Settings.Default.password;
+            playerNameField.Text = Properties.Settings.Default.playerName;
+            gameNameField.Text = Properties.Settings.Default.gameName;
+            serverIpField.Text = Properties.Settings.Default.serverIp;
+            serverPortField.Text = Properties.Settings.Default.serverPort.ToString();
+            passwordField.Text = Properties.Settings.Default.password;
 
-            _settings = new ClientSettings(playerName, gameName, serverIp, serverPort, password);
-            playerNameField.Text = playerName;
-            // Set game name
-            serverIpField.Text = serverIp;
-            serverPortField.Text = serverPort > 0 ? serverPort.ToString() : string.Empty;
-            passwordField.Text = password;
+            _settings = ValidateInputFields();
         }
 
         /// <summary>
@@ -109,11 +106,12 @@ namespace Windwaker.Multiplayer.Client
         /// </summary>
         private void OnFormClose(object sender, FormClosingEventArgs e)
         {
-            Properties.Settings.Default.playerName = _settings.playerName;
-            Properties.Settings.Default.gameName = _settings.gameName;
-            Properties.Settings.Default.serverIp = _settings.serverIp;
-            Properties.Settings.Default.serverPort = _settings.serverPort;
-            Properties.Settings.Default.password = _settings.password;
+            ClientSettings settings = ValidateInputFields();
+            Properties.Settings.Default.playerName = settings.TruePlayerName;
+            Properties.Settings.Default.gameName = settings.TrueGameName;
+            Properties.Settings.Default.serverIp = settings.TrueServerIp;
+            Properties.Settings.Default.serverPort = settings.TrueServerPort;
+            Properties.Settings.Default.password = settings.Password;
             Properties.Settings.Default.Save();
         }
 
